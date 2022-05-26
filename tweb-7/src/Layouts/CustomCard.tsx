0f8604c24b06a9d61ebcae968c29222ec9a10c9d
Petru <57636887+MasterRoka@users.noreply.github.com>
2022-05-26 22:39:19 +0300

    Add files via upload

---
+++ tweb-7/src/Layouts/CustomCard.tsx
@@ -0,0 +1,33 @@
+import {Button, Card, Checkbox, Col, Input} from 'antd'
+import { IContentModel} from "../interface/Interfaces";
+import {useRootStore} from "../index";
+import {observer} from "mobx-react-lite";
+import {useEffect} from "react";
+//CustomCard
+export const CustomCard=observer(({content}:{content:IContentModel}) => {
+
+    const {title,description,notite,changeNotes,isSeen,changeSeen,addNoteValue,removeNote} =content
+    useEffect(()=>{
+        if(isSeen) {
+            console.log(`Is seen ${ title }`)
+        }
+        },[isSeen])
+        return (
+
+            <Card title={title} bordered={false}>
+                {description}
+                <Input value={notite}
+                       placeholder={notite}
+                       onChange={(e)=>changeNotes(e.target.value)}
+                />
+                <Checkbox defaultChecked={ isSeen } checked={isSeen} onChange={(e)=>changeSeen(e.target.checked)}/>
+                 <Button onClick={addNoteValue}>Add value</Button>
+                <Button type={'primary'} onClick={removeNote}>Remove value</Button>
+            </Card>
+
+
+    )
+}
+)
+
+
