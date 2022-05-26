2f1cfb874d02fc8bf963de635316ddd4f54c4d27
Petru <57636887+MasterRoka@users.noreply.github.com>
2022-05-26 22:42:02 +0300

    Add files via upload

---
+++ tweb6/src/Layouts/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+export const Home = () =>{
+    return(
+        <main>
+            <body>
+            <pre>
+    |‾‾‾‾‾‾‾‾‾‾‾|<pre></pre>
+    | Aloha     |<pre></pre>
+    | server    |<pre></pre>
+    | is ONN    |<pre></pre>
+    |___________|<pre></pre>
+            ||<pre></pre>
+    (\__/) ||<pre></pre>
+    (•ㅅ•) ||<pre></pre>
+    / 　 づ
+  </pre>
+            </body>
+        </main>
+    )
+}
